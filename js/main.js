06fcb7637d19bf23eb464487814838a3c3dcfee1
Vlad Velici <vlad.velici@gmail.com>
2013-09-14 18:13:18 +0300

    vlad's map fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 (function() {
 	var height;
 
+	var map;
+	var mapinit = function() {
+		var mountbatten = new google.maps.LatLng(50.937281, -1.398491);
+		var mapOptions = {
+			zoom: 15,
+			center: mountbatten,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		map = new google.maps.Map(document.getElementById('location_map'), mapOptions);
+
+		new google.maps.Marker({
+			position: mountbatten,
+			map: map,
+			title: "Mountbatten Building"
+		});
+	}
+
 	var resizeDiv = function() {
 		var nh = window.innerHeight;
 		if (height === nh) return;
@@ -11,10 +28,12 @@
 
 	window.addEventListener("load", function() {
 		resizeDiv();
+		mapinit();
 	});
 
 	window.addEventListener("resize", function() {
 		resizeDiv();
-	})
+	});
+
 
 })();
